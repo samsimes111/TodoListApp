924c8abb829f2eb3701ca321cc4c88a8b8472b05
Yuuu Bin <daonguyennhatanh0910@gmail.com>
2024-07-23 21:02:11 +0700

    delete file#

--- ProjectTodoListApp/ProjectTodoListApp/Program.cs
+++ ProjectTodoListApp/ProjectTodoListApp/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!cccccc");
+            Console.WriteLine("Hello World!ccc");
         }
     }
 }
