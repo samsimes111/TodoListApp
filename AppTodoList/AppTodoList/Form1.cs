7b45c286ec12d8435bf5151fa37a5c27eb692329
Yuuu Bin <daonguyennhatanh0910@gmail.com>
2024-07-27 20:59:58 +0700

    Update : File Mains

--- AppTodoList/AppTodoList/Form1.cs
+++ AppTodoList/AppTodoList/Form1.cs
@@ -1,7 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace AppTodoList
 {
     public partial class Form1 : Form
     {
+        private List<Task> tasks = new List<Task>();
         public Form1()
         {
             InitializeComponent();
@@ -15,17 +20,35 @@ namespace AppTodoList
 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         private void addButton_Click(object sender, EventArgs e)
         {
+            string task = textBox1.Text.Trim();
+            DateTime startDate = dateTimePicker1.Value;
+            DateTime endDate = dateTimePicker2.Value;
+            if (!string.IsNullOrEmpty(task))
+            {
+                dataGridView1.Rows.Add(task, startDate.ToShortDateString(), endDate.ToShortDateString());
+                textBox1.Clear();
+            }
 
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void todayButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
 
         }
